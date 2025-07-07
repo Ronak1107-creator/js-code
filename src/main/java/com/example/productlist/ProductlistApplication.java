25fc0d9fca54e0dbbdc3354791404492de3f543f
@ronak.jain <ronak.jain@synoriq.in>
2025-07-07 22:40:10 +0530

    intial commit

---
+++ src/main/java/com/example/productlist/ProductlistApplication.java
@@ -0,0 +1,15 @@
+package com.example.productlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class ProductlistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductlistApplication.class, args);
+	}
+
+}
